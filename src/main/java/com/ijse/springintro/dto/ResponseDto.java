ae6fd7d283e670f25d07ca9b8d2ebac9c1cee5dc
Hashitha097 <hashithaheshan097@gmail.com>
2024-07-29 11:24:40 +0530

    first

---
+++ src/main/java/com/ijse/springintro/dto/ResponseDto.java
@@ -0,0 +1,14 @@
+package com.ijse.springintro.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class ResponseDto {
+    private String message;
+
+
+}
---
+++ src/main/java/com/ijse/springintro/service/TaskService.java
@@ -0,0 +1,18 @@
+package com.ijse.springintro.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ijse.springintro.entity.Task;
+
+@Service
+public interface TaskService {
+    //what do we expect to do with Task
+    List<Task>getTasksList(); // reading
+    Task createTask(Task task); // creting
+    Task getTaskById(Long id); // finding a task by it's primary key
+    Task updateTask(Long id, Task task); // find task and update
+    void deleteTask(Long id); // delete task
+
+}
